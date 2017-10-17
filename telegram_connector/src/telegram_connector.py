29dcb3c6bd0104b1c97d916be1a6fbcb04e43727
aybulat-PProger <aibulatb24@gmail.com>
2017-10-17 21:49:09 +0400

    Telegram connector

---
+++ telegram_connector/src/telegram_connector.py
@@ -0,0 +1,24 @@
+import requests
+import const
+import json
+
+URL = "https://api.telegram.org/bot"+const.token
+
+
+def get_updates_json():#получение обновлений
+    response = requests.get(URL + '/getUpdates')
+    return response.json()
+
+def get_userdata():#получение ид пользователя и текста собщения
+    userdata = get_updates_json()
+    user_id = userdata['result'][-1]['message']['from']['id']
+    user_text = userdata['result'][-1]['message']['text']
+    data = {'user_id': user_id, 'user_text': user_text }
+    file = open('updates.txt', 'a')
+    file.write('id = ' + str(data['user_id']) + '\n\ttext = '+str(data['user_text'])+'\n')
+    file.close()
+    return data
+
+def send_message(chat_id, text):#отправка сообщений
+    url = URL + '/sendMessage?chat_id='+str(chat_id)+'&text='+text
+    requests.get(url)
