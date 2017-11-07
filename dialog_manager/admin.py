ee75c9465ab154d8ccab08f8f7acc1579b12f16f
Dmitry <rdima.96@yandex.ru>
2017-11-07 20:19:39 +0400

    Поправил баги а админ панели

--- dialog_manager/admin.py
+++ dialog_manager/admin.py
@@ -1,39 +1,29 @@
 from django.contrib import admin
-from .models import*
+from .models import *
 
-class InputSentenceAdmin (admin.StackedInline):
+
+class InputSentenceInline(admin.StackedInline):
     # показывать текст предложений
-    fields = ("text",)
     model = InputSentence
     extra = 1
 
+
 class IntentAdmin(admin.ModelAdmin):
     # показывать входящие в интент предложения
-    inlines = [InputSentenceAdmin,]
+    inlines = [InputSentenceInline, ]
 
-class IntentInLine(admin.StackedInline):
-    model = Intent
-    extra = 1
 
-class ResponseInLine (admin.StackedInline):
-    # показывать текст ответа
-    fields = ("text",)
-    model = Pair
-    extra = 1
+class PairAdmin(admin.ModelAdmin):
+    pass
 
-class PairAdmin (admin.ModelAdmin):
-# показывать связанные интенты и ответы
-    inlines = [ResponseInLine,  IntentInLine,]
 
 class DialogHistoryAdmin(admin.ModelAdmin):
     # просто отображать поля
-    readonly_fields =("chat_id", "text","source","date",)
+    readonly_fields = ("chat_id", "text", "source", "date",)
     actions = None
+
 
 admin.site.register(Intent, IntentAdmin)
 admin.site.register(Pair, PairAdmin)
-admin.site.register(DialogHistory)
-#admin.site.register(Users)
-#admin.site.register(DisplayedMessages)
-#admin.site.register(NotAnsweredMessage)
+admin.site.register(DialogHistory, DialogHistoryAdmin)
 # Register your models here.
